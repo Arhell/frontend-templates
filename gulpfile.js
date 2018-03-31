6582de070d34307382baf7540950ce76241850e8
Arhell <arhell333@gmail.com>
2018-04-01 00:54:12 +0300

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp           = require('gulp'),
-		gutil          = require('gulp-util' ),
 		sass           = require('gulp-sass'),
 		browserSync    = require('browser-sync'),
 		concat         = require('gulp-concat'),
