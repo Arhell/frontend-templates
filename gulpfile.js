f42ff364ae673fb76ce938295bd7980db0c3ba7d
Arhell <arhell333@gmail.com>
2018-04-09 21:59:20 +0300

    add first task

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,10 @@
 var gulp = reguire('gulp'),
 		glp  = reguire('gulp-load-plugins');
 
+gulp.task('pug', function () {
+	return gulp.src('src/pug/*.pug')
+		.pipe(glp.pug({
+			pretty:true
+		}))
+		.pipe(gulp.dest('dist'));
+});
