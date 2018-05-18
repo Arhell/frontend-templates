caf1ca0093dabde5f15404f25d7751747bb5ae66
Arhell <arhell333@gmail.com>
2018-05-19 01:55:17 +0300

    upd path

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('stylus', function () {
 });
 
 gulp.task('script', function () {
-	return gulp.src('src/scripts/*.js')
+	return gulp.src('src/js/*.js')
 		.pipe(glp.concat('dist/js/scripts.min.js'))
 		.pipe(bs.reload ({
 			stream: true
