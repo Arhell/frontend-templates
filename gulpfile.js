98ae955760acf627ef0f47eb4ca61993cf668499
Arhell <arhell333@gmail.com>
2018-05-23 00:14:17 +0300

    add imagemin task

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,12 @@ gulp.task('scripts', function () {
 		}));
 });
 
+gulp.task('img', () =>
+	gulp.src('src/img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/images'))
+);
+
 gulp.task('bs', function() {
 	bs.init({
 		server: {
