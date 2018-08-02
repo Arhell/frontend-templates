fbb92da137f8141623fda20271037c3ee3af75cd
Arhell <arhell333@gmail.com>
2018-08-03 00:38:36 +0300

    upd img

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,7 +1,7 @@
 module.exports = function () {
-  gulp.task('img', function () {
-    return gulp.src('src/img/*.{png, jpg, gif}')
-      .pipe(glp.imagemin())
-      .pipe(gulp.dest('dist/img'));
+  $.gulp.task('img', function () {
+    return $.gulp.src('src/img/*.{png, jpg, gif}')
+      .pipe($.glp.imagemin())
+      .pipe($.gulp.dest('dist/img'));
   });
 };
