e3f016dd79c837fb1776fab5935dcda52fe6a56a
Arhell <arhell333@gmail.com>
2019-01-13 19:53:33 +0200

    small ref

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ global.$ = {
   gulp: require('gulp'),
   glp:  require('gulp-load-plugins')(),
   bs:   require('browser-sync').create(),
-
   path: {
     tasks: require('./gulp/path/tasks')
   }
@@ -13,8 +12,6 @@ global.$ = {
 $.path.tasks.forEach(function (taskPath) {
   require(taskPath)();
 });
-
-
 
 $.gulp.task('default', $.gulp.series(
   $.gulp.parallel('pug', 'stylus', 'scripts', 'img'),
