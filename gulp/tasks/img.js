4e6432de1133d01a57fe24f6525a2064ee7e05ca
Arhell <arhell333@gmail.com>
2019-01-16 01:57:32 +0200

    upd task img

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,7 +1,10 @@
 module.exports = function () {
   $.gulp.task('img', function () {
     return $.gulp.src('src/img/*.{png, jpg, gif}')
-      .pipe($.glp.imagemin())
+      .pipe($.glp.imagemin(
+        [imageminMozjpeg()],
+        {verbose: true}
+      ))
       .pipe($.gulp.dest('dist/img'));
   });
 };
