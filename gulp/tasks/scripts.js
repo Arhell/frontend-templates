e4df7f3eb9684be16cb05f07874549798cc466f7
Arhell <arhell333@gmail.com>
2019-02-04 00:30:34 +0200

    upd scripts task

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,8 +1,6 @@
 module.exports = function () {
   $.gulp.task('scripts', function () {
-    return $.gulp.src(['src/js/libs.js',
-      'src/js/common.js'
-    ])
+    return $.gulp.src(['src/js/*.js'])
       .pipe($.glp.concat('scripts.min.js'))
       .pipe($.gulp.dest('dist/js'))
       .pipe($.bs.reload({
